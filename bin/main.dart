eccae229c3c438a3aefdb8f316304bbbea4da9f2
renyuxiang <renyuxiang@waijiaoyi.com>
2018-09-14 17:50:52 +0800

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:DartFirst/DartFirst.dart' as DartFirst;
+
+main(List<String> arguments) {
+  print('Hello world: ${DartFirst.calculate()}!');
+}
---
+++ test/DartFirst_test.dart
@@ -0,0 +1,8 @@
+import 'package:DartFirst/DartFirst.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
